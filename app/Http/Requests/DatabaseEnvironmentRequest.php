e74f233d825a5ea3be088412c7097a06a2cf9b3c
Nathanael McDaniel <leemcd56@gmail.com>
2019-11-21 18:18:36 -0600

    Removed max length for database hostname

--- app/Http/Requests/DatabaseEnvironmentRequest.php
+++ app/Http/Requests/DatabaseEnvironmentRequest.php
@@ -26,7 +26,7 @@ class DatabaseEnvironmentRequest extends FormRequest
         return [
             'app_url'               => 'required|url',
             'database_connection'   => 'required|string|max:50',
-            'database_hostname'     => 'required|string|max:50',
+            'database_hostname'     => 'required|string',
             'database_port'         => 'required|numeric',
             'database_name'         => 'required|string|max:50',
             'database_username'     => 'required|string|max:50',
